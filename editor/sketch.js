bddfde19be4ac04e60af4e131a010308467155e5
Vini <desegames132@gmail.com>
2026-09-24 19:04:49 -0300

    feat: add function matematicalFlowe

--- editor/sketch.js
+++ editor/sketch.js
@@ -18,4 +18,25 @@ export function drawReceipt(p){
       y: p.sin(angle) * radius,
     };
   }
+
+  function mathematicalFlowe(radius, petals, amplitude, rotation = 0){
+    p.beginShape();
+
+    const steps = 700;
+
+    for(let i=0;i<=steps;i++){
+      const angle = p.map(i, 0, steps, 0, p.PI);
+
+      const wave1 = p.sin(petals * angle + rotation);
+      const wave2 = p.sin((petals * 2) * angle) * 0.15;
+
+      const r = radius * (1+ amplitude * wave1 + amplitude * wave2);
+      
+      const point = polar(angle, r);
+
+      p.vertex(point.x, point.y);
+    }
+
+    p.endShape();
+  }
 }
